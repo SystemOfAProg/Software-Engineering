b3e5f7b0898c010f629e18d3685675f8814555ff
Unknown <fabian.sorn@online.de>
2018-03-17 18:35:54 +0100

    include main-method for starting the application

---
+++ workspace/Desgin_und_Implementierung/src/application/IWWWDNW.java
@@ -0,0 +1,11 @@
+package application;
+
+// Start for application "Ich weiß, was was du nicht weißt."
+public class IWWWDNW {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
