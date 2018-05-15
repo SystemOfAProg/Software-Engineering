1999597275fee35824f628fbec144f06db1c5e2f
Unknown <fabian.sorn@online.de>
2018-05-15 15:54:49 +0200

    Übernahme States aus MyClubDesktop

--- Implementation/src/application/logic/stateMachine/port/IState.java
+++ Implementation/src/application/logic/stateMachine/port/IState.java
@@ -1,5 +1,45 @@
 package application.logic.stateMachine.port;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public interface IState {
+	
+	public boolean isSubStateOf(IState state);
+
+	public boolean isSuperStateOf(IState state);
+
+	public enum State implements IState {
+
+		Started, ValidationFailed, Login(Started, ValidationFailed), //
+		SelectUseCase, EditMember, NotAllowed, ManageMembers(EditMember, NotAllowed), //
+		Initialized(SelectUseCase, ManageMembers);
+
+		/**
+		 * @clientNavigability NAVIGABLE
+		 * @directed true
+		 * @supplierRole subState
+		 */
+
+		private List<IState> subStates;
+
+		private State(IState... subS) {
+			this.subStates = new ArrayList<>(Arrays.asList(subS));
+		}
+
+		@Override
+		public boolean isSuperStateOf(IState s) {
+			boolean result = s == null || this == s; // self contained
+			for (IState state : this.subStates) // or
+				result |= state.isSuperStateOf(s); // contained in a substate!
+			return result;
+		}
+
+		@Override
+		public boolean isSubStateOf(IState state) {
+			return (state == null) ? false : state.isSuperStateOf(this);
+		}
+	}
 	
 }
